d43e88527ec2d245b4626987e652ff00ce3b9aa6
KuTear <kutear.com@gmail.com>
2015-08-11 14:51:41 +0800

    引入左滑关闭

--- app/src/main/java/com/kutear/app/activity/BaseActivity.java
+++ app/src/main/java/com/kutear/app/activity/BaseActivity.java
@@ -1,15 +1,22 @@
 package com.kutear.app.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public abstract class BaseActivity extends AppCompatActivity {
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+public abstract class BaseActivity extends SwipeBackActivity {
+
+    private SwipeBackLayout mSwipeBackLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSwipeBackLayout = getSwipeBackLayout();
+        //允许左侧滑动返回
+        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
     }
 
     @Override
